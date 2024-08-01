75277c66b8f2550342f5e700732669f1c11f1c64
dhvani-dhondiyal <69759249+dhvani-dhondiyal@users.noreply.github.com>
2024-08-01 16:51:27 +0530

    First commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+}
+
